8ad3f99ecafc3b10d18eac577910ac074b4e9f0e
woodtop <cwood89@mac.com>
2018-10-31 12:20:02 -0400

    added HTML code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,13 @@
+
+
+
 var right = 0;
 var wrong = 0;
 var time;
 
 $(document).ready(function(){
     $('#btnGetValue').click(function() {
-        var selValue = $('input[name=rbnNumber]:checked').val(); 
+        var selValue = $('input[name=questionOne]:checked').val(); 
         $('p').html('<br/>Selected Radio Button Value is : <b>' + selValue + '</b>');
     });
 });
