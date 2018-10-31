55b9e31990f8376c43a913f78f82b1973b93158f
woodtop <cwood89@mac.com>
2018-10-31 12:45:01 -0400

    finished html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,12 @@
-
-
-
+var answerArr = ["Captain America", "Catwoman", "The Green Lantern", "Storm", "Spiderman",
+"Daredevil", "The Beast", "Star City"];
 var right = 0;
 var wrong = 0;
 var time;
 
 $(document).ready(function(){
-    $('#btnGetValue').click(function() {
-        var selValue = $('input[name=questionOne]:checked').val(); 
-        $('p').html('<br/>Selected Radio Button Value is : <b>' + selValue + '</b>');
+    $("#questionOne").click(function() {
+        var answer = $('input[name=questionOne]:checked').val(); 
+         console.log(answer);
     });
 });
