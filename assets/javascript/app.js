1c9f7b9a077bb6254778c8990032f7e8e487c1be
woodtop <cwood89@mac.com>
2018-10-31 14:35:34 -0400

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,43 @@ var answerArr = ["Captain America", "Catwoman", "The Green Lantern", "Storm", "S
 "Daredevil", "The Beast", "Star City"];
 var right = 0;
 var wrong = 0;
-var time;
+var time = 30;
+var guess = {
+    one: $('input[name=questionOne]:checked').val(), 
+    two: $('input[name=questionTwo]:checked').val(),
+    three: $('input[name=questionThree]:checked').val(),
+    four: $('input[name=questionFour]:checked').val(),
+    five: $('input[name=questionFive]:checked').val(),
+    six: $('input[name=questionSix]:checked').val(),
+    seven: $('input[name=questionSeven]:checked').val(),
+    eight: $('input[name=questionEight]:checked').val(),
+};
 
-$(document).ready(function(){
-    $("#questionOne").click(function() {
-        var answer = $('input[name=questionOne]:checked').val(); 
-         console.log(answer);
-    });
-});
+function timeCount() {
+    time--;
+    $("#time").text("Time Remaining: " + time);
+    console.log(guess.one);
+};
+
+function checkAnswers() {
+    for(var i = 0; i < answerArr.length; i++) {
+        if (guessArr[i] === answerArr[i]) {
+            right++;
+        } else {
+            wrong++;
+        }
+    };
+};
+function scoreCheck() {
+    checkAnswers();
+    $("#questions").empty();
+    var r = $("<p>");
+    var w = $("<p>");
+    r.text("Correct Answers: " + right);
+    w.text("Wrong Answers: " + wrong);
+    $("#main-container").append(r, w);
+};
+setTimeout(scoreCheck, 30000);
+setInterval(timeCount, 1000);
+
+
